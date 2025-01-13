aeffd20d2c8fa8a6915ace13b90f8b5e33d17d8a
lkb <lkb@tascorp.co.kr>
2025-01-13 09:28:22 +0900

    [등록] 1. winforms에서 aop 테스트 - 로그

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            LoadData();
+            SaveData();
+        }
+        [LogAttritas] // AOP 적용
+        public void LoadData()
+        {
+            Console.WriteLine("데이터 로드 중...");
+            System.Threading.Thread.Sleep(500);
+        }
+
+        [LogAttritas] // AOP 적용
+        public void SaveData()
+        {
+            Console.WriteLine("데이터 저장 중...");
+            System.Threading.Thread.Sleep(300);
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/LogAttribute.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MethodDecorator.Fody.Interfaces;
+
+namespace WindowsFormsApp1
+{
+    [AttributeUsage(AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Assembly)]
+    public class LogAttritas : Attribute, IMethodDecorator
+    {
+        private DateTime _startTime;
+
+        // 메서드 호출 전 실행
+        public void Init(object instance, System.Reflection.MethodBase method, object[] args)
+        {
+            Console.WriteLine($"메서드 {method.Name} 호출 시작");
+            _startTime = DateTime.Now;
+        }
+
+        // 메서드 실행 후 실행
+        public void OnExit()
+        {
+            var duration = DateTime.Now - _startTime;
+            Console.WriteLine($"메서드 종료. 실행 시간: {duration.TotalMilliseconds}ms");
+        }
+
+        // 예외 발생 시 실행
+        public void OnException(Exception exception)
+        {
+            Console.WriteLine($"예외 발생: {exception.Message}");
+        }
+
+        public void OnEntry()
+        {
+
+        }
+    }
+}
